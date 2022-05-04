7ca096fa83ff13ce467e2c1c913c09a787d25cad
Pratham Soni <psoni@DN0a23287e.SUNet>
2022-05-04 10:19:52 -0700

    add wilds repo

---
+++ scripts/reproduce.sh
@@ -0,0 +1,6 @@
+source activate pytorch_latest_p37
+pip install -r requirements.txt
+python setup.py install
+python examples/run_expt.py --dataset iwildcam --algorithm deepCORAL --root_dir data --log_dir ./logs/iwildcam --download true
+python examples/run_expt.py --dataset globalwheat --algorithm ERM --root_dir data --log_dir ./logs/globalwheat --download true
+python examples/run_expt.py --dataset poverty --algorithm ERM --root_dir data --log_dir ./logs/poverty --download true
